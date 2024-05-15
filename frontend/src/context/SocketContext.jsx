ea0c33ed92a0c1b851c05ab3d11c37431018ac58
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-05-15 14:48:01 +0530

    added socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
       if(authUser){
-        const socket = io("http://localhost:5000", {
+        const socket = io("https://chat-application-r1o0.onrender.com", {
             query: {
                 userId: authUser.data._id
             }
